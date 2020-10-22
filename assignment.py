8fc5c229db0cbc057ae8552a1c0f77b7f6c5b74a
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-22 13:02:47 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -10,21 +10,21 @@ while doLoop:
     a=a+1
     if count==50:
         count=0
-        while p.pixelMatchesColor(1679,999,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1679,999,(255,255,255),tolerance=0.9):
             p.click(1679,999)
-        while p.pixelMatchesColor(1676,935,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1676,935,(255,255,255),tolerance=0.9):
             p.click(1676,935)
-        while p.pixelMatchesColor(1674,876,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1674,876,(255,255,255),tolerance=0.9):
             p.click(1674,876)
-        while p.pixelMatchesColor(1678,805,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1678,805,(255,255,255),tolerance=0.9):
             p.click(1678,805)
-        while p.pixelMatchesColor(1676,741,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1676,741,(255,255,255),tolerance=0.9):
             p.click(1676,741)
-        while p.pixelMatchesColor(1674,682,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1674,682,(255,255,255),tolerance=0.9):
             p.click(1674,682)
-        while p.pixelMatchesColor(1674,616,(255,255,255),tolerance=10):
+        while p.pixelMatchesColor(1674,616,(255,255,255),tolerance=0.9):
             p.click(1674,616)
-    if a == 50:
+    if a == 100:
         a=0
         if p.confirm(text="Do you want to stop the program?",title="",buttons=["OK","Continue"])=="OK":
             doLoop=False
